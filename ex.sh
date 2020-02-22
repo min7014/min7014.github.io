3e4a76ce235322810c853b3794c37bc835288206
min7014 <30566415+min7014@users.noreply.github.com>
2020-02-22 19:13:35 +0900

    a

---
+++ ex.sh
@@ -0,0 +1,4 @@
+  
+rm ex.sh -f
+sudo wget min7014.github.io/raspberrypi/files/2019122901.sh
+sh 2019122901.sh
---
+++ rasp.sh
@@ -0,0 +1,3 @@
+rm rasp.sh -f
+sudo wget min7014.github.io/raspberrypi/files/2019112802.sh 
+sh 2019112802.sh 
